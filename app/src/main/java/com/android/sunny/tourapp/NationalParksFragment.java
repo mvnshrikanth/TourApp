2c411cce4601dbc98384c050763247b58c47dba1
Shrikanth Malarouth <mvnshrikanth@gmail.com>
2016-11-30 01:34:59 -0500

    Adding string resources in progress

--- app/src/main/java/com/android/sunny/tourapp/NationalParksFragment.java
+++ app/src/main/java/com/android/sunny/tourapp/NationalParksFragment.java
@@ -22,8 +22,13 @@ public class NationalParksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.location_list, container, false);
+        View rootView = inflater.inflate(R.layout.location_list, container, false);
+
+
+        return rootView;
+
     }
 
 }
